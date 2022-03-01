2e8e80154602f0cacac9c6b83e2eff219c260773
Romana Černá <Cerna.Romana@stud.vassboskovice.cz>
2022-03-01 09:39:34 +0100

    Přidejte soubory projektu.

---
+++ OOP_clovek/clovek.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_clovek
+{
+    public class clovek
+    {
+        public string Jmeno { get; set; }
+        public string Prijmeni { get; set; }
+        public DateTime DatumNarozeni { get; set; }
+        private int energie;
+        public int Energie { get; set; }
+        public int Zivot { get; set; }
+        private int dovednost;
+        public int Dovednost { get; set; }
+
+        public clovek()
+        {
+            Jmeno = "";
+            Prijmeni = "";
+            DatumNarozeni = DateTime.Now;
+            energie = 100;
+            Zivot = 100;
+            dovednost = 0;
+        }
+
+        public void ZvysitDovednost()
+        {
+            Dovednost += 20;
+            Energie += 10;
+        }
+    }
+}
